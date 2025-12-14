bc7231d79a2272099f125e4c48f7c4bef1ba360e
Adrián Álvarez Calderón <9157735+adalvarez@users.noreply.github.com>
2025-12-14 17:41:55 +0000

    setting up django rest framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-!fu**$*9gl7%1v$az=+hsvjl%wvpqi&-cbzdzm15)lk2rflz&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
